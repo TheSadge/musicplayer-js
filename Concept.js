c160eff1612756c6614c2d8819122e879939b36c
Sajjad Hakemzade <149905781+TheSadge@users.noreply.github.com>
2025-01-10 18:14:29 +0330

    Add files via upload

---
+++ Concept.js
@@ -0,0 +1,35 @@
+// **Player Area Selection**
+// select all layers
+
+// select music path
+
+// select button layers
+
+// select all control button
+
+// set media current time
+
+// access to media full length
+
+// Select the progress bar element
+
+// **Play Button Event Listener**
+// runs function after interaction with play
+
+// **Toggle Player Icon**
+// dynamic player button
+
+// reset icons after media ends
+
+// **Render Music (Play/Pause)**
+
+// **Get Time Format**
+// calculate media length
+
+// **Rewind and Forward Buttons Functionality**
+// buttons function
+
+// **Update Current Time and Progress Bar**
+// update current media time and bar style
+
+// **Progression Bar Input Event Listener**
